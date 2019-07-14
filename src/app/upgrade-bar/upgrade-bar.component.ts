96b8f81affcc5835fb266182a2eae274425da9be
Thomas Micol <thomasmicol@gmail.com>
2019-07-14 22:05:35 +1200

    ADded the upgrade effect enum

--- src/app/upgrade-bar/upgrade-bar.component.ts
+++ src/app/upgrade-bar/upgrade-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Upgrade } from '../upgrade'
+import { Upgrade } from '../upgrade';
+import { EffectTargetEnum } from '../effect-target-enum.enum';
 
 @Component({
   selector: 'app-upgrade-bar',
@@ -16,8 +17,8 @@ export class UpgradeBarComponent implements OnInit {
 
     ngOnInit() {
         this.availableUpgrades = [
-            new Upgrade("New Mouse", 10),
-            new Upgrade("New Computer", 20),
+            new Upgrade("New Mouse", 10, EffectTargetEnum.FlatClickGain, 10),
+            new Upgrade("New Computer", 20, EffectTargetEnum.ClickGainMultiplier, 2),
         ]
 
     }
--- src/app/upgrade.ts
+++ src/app/upgrade.ts
@@ -1,9 +1,19 @@
+import { EffectTargetEnum } from './effect-target-enum.enum';
+
 export class Upgrade {
     Name : string;
     Cost : number;
+    public EffectTarget : EffectTargetEnum;
+    public EffectNumber : number;
 
-    constructor(name: string, cost: number){
+    constructor(name: string, 
+        cost: number, 
+        target: EffectTargetEnum, 
+        effectNumber: number)
+        {
         this.Name = name;
         this.Cost= cost;
+        this.EffectTarget = target;
+        this.EffectNumber = effectNumber;
     }
 }
