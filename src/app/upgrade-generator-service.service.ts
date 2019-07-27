636a68f717a6ad85922d45c6a39dbc89404bf053
Thomas Micol <thomasmicol@gmail.com>
2019-07-27 22:58:54 +1200

    added service explaination

--- src/app/upgrade-generator-service.service.ts
+++ src/app/upgrade-generator-service.service.ts
@@ -3,6 +3,15 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
+/**
+ * This service is used to generate upgrades as they are needed by the user
+ * as a user buys an upgrade we need to replace the purchased upgrade with a
+ * newer and more effective upgrade. 
+ * 
+ * The generated upgrade needs to have its price scale with the amount of money 
+ * user makes in a click
+*/
 export class UpgradeGeneratorServiceService {
 
   constructor() { }
